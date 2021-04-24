42f663963c8da810d0eefa1b2669ae548dc1a104
sjjeong <sjjeong1225@gmail.com>
2021-04-24 11:53:52 +0900

    Detail에 곡 정보 보여주기

--- iTunesSearchRxMvvm/ui/detail/DetailViewController.swift
+++ iTunesSearchRxMvvm/ui/detail/DetailViewController.swift
@@ -5,8 +5,58 @@
 //  Created by SeokJun Jeong on 2021/04/24.
 //
 
-import Foundation
+import UIKit
+import RxCocoa
+import RxSwift
+import Kingfisher
 
 final class DetailViewController: BaseViewController<DetailViewModel> {
+ 
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var tradeNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupViewModelBind()
+    }
+    
+    private func setupViewModelBind() {
+        viewModel.searchInfo
+            .map { $0.imageUrl }
+            .flatMap { imageUrl in
+                Observable.of(imageUrl, imageUrl.replacingOccurrences(of: "100x100", with: "200x200"))
+            }
+            .flatMap { imageUrl in
+                Observable.create { observer in
+                    guard let url = URL(string: imageUrl) else {
+                        return Disposables.create()
+                    }
+                    let imageResource = ImageResource(downloadURL: url)
+                    KingfisherManager.shared.retrieveImage(with: imageResource) { result in
+                        switch result {
+                        case .success(let value):
+                            observer.on(.next(value.image))
+                        case .failure(let error):
+                            observer.on(.error(error))
+                        }
+                    }
+                    return Disposables.create()
+                }
+            }
+            .asDriver(onErrorDriveWith: Driver.empty())
+            .drive(trackImageView.rx.image)
+            .disposed(by: disposeBag)
+        viewModel.searchInfo
+            .map { $0.trackName }
+            .asDriver(onErrorDriveWith: Driver.empty())
+            .drive(tradeNameLabel.rx.text)
+            .disposed(by: disposeBag)
+        viewModel.searchInfo
+            .map { $0.artistName }
+            .asDriver(onErrorDriveWith: Driver.empty())
+            .drive(artistNameLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
     
 }
